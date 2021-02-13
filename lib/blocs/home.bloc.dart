0fb70185ee1bd7bab401b208df3269fedbf86fc3
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-13 11:25:46 -0300

    Aula Home BloC

---
+++ lib/blocs/home.bloc.dart
@@ -0,0 +1,42 @@
+import '../models/category-list-item.model.dart';
+import '../models/product-list-item.model.dart';
+import '../repositories/category.repository.dart';
+import '../repositories/product.repository.dart';
+
+class HomeBloc {
+  final categoryRepository = new CategoryRepository();
+  final productRepository = new ProductRepository();
+
+  List<ProductListItemModel> products;
+  List<CategoryListItemModel> categories;
+  String selectedCategory = 'todos';
+
+  HomeBloc() {
+    getCategories();
+    getProducts();
+  }
+
+  getCategories() {
+    categoryRepository.getAll().then((data) {
+      this.categories = data;
+    });
+  }
+
+  getProducts() {
+    productRepository.getAll().then((data) {
+      this.products = data;
+    });
+  }
+
+  getProductsByCategory() {
+    productRepository.getByCategory(selectedCategory).then((data) {
+      this.products = data;
+    });
+  }
+
+  changeCategory(tag) {
+    selectedCategory = tag;
+    products = null;
+    getProductsByCategory();
+  }
+}

8b57fcbb7f02eb9a2e7ad8c43d8ee79406fc27e0
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-13 11:06:13 -0300

    Aula Category Repository

---
+++ lib/repositories/category.repository.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:shopping_car_app/models/category-list-item.model.dart';
+import '../settings.dart';
+
+class CategoryRepository {
+  Future<List<CategoryListItemModel>> getAll() async {
+    var url = "${Settings.apiUrl}v1/categories";
+
+    Response response = await Dio().get(url);
+
+    return (response.data as List)
+        .map((course) => CategoryListItemModel.fromJson(course))
+        .toList();
+  }
+}
